c2528068db01bae5d665b6b33bfcf6496e486cf0
Saly <saly@juke4me.com>
2019-05-31 20:34:50 +0530

    Complet ConsumerRole

--- contracts/coffeeaccesscontrol/ConsumerRole.sol
+++ contracts/coffeeaccesscontrol/ConsumerRole.sol
@@ -6,49 +6,48 @@ import "./Roles.sol";
 // Define a contract 'ConsumerRole' to manage this role - add, remove, check
 contract ConsumerRole {
 
+  using Roles for Roles.Role;
   // Define 2 events, one for Adding, and other for Removing
-  event add(address account);
-  event remove(address account);
+  event ConsumerAdd(address account);
+  event ConsumerRemove(address account);
   // Define a struct 'consumers' by inheriting from 'Roles' library, struct Role
-  struct consumers  {
-    address account;
-    mapping (address => Roles) role;
-  }
+  Roles.Role private consumers;
 
-  mapping (uint => consumers) consumer;
   // In the constructor make the address that deploys this contract the 1st consumer
   constructor() public {
-    consumer[0] = consumers(msg.sender);
+    _addConsumer(msg.sender);
   }
 
   // Define a modifier that checks to see if msg.sender has the appropriate role
   modifier onlyConsumer() {
-  //  require(consumers(msg.sender) == consumers.role[msg.sender], "You have an inappropriate role.");
+    require(isConsumer(msg.sender), "You have an inappropriate role.");
     _;
   }
 
   // Define a function 'isConsumer' to check this role
   function isConsumer(address account) public view returns (bool) {
-    
+   return consumers.has(account);
   }
 
   // Define a function 'addConsumer' that adds this role
   function addConsumer(address account) public onlyConsumer {
-    
+   _addConsumer(account);
   }
 
   // Define a function 'renounceConsumer' to renounce this role
   function renounceConsumer() public {
-    
+    _removeConsumer(msg.sender);
   }
 
   // Define an internal function '_addConsumer' to add this role, called by 'addConsumer'
   function _addConsumer(address account) internal {
-    
-  }
+    consumers.add(account);
+    emit ConsumerAdd(account);
+    }
 
   // Define an internal function '_removeConsumer' to remove this role, called by 'removeConsumer'
   function _removeConsumer(address account) internal {
-    
+    consumers.remove(account);
+    emit ConsumerRemove(account);
   }
 }
