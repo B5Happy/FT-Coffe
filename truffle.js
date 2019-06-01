feed606a312fc78f6f7d8e6608a6ddcf0d925866
Saly <saly@juke4me.com>
2019-06-02 05:20:50 +0530

    update view

--- truffle.js
+++ truffle.js
@@ -1,9 +1,18 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
 module.exports = {
   networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*" // Match any network id
-    }
-  }
+    // development: {
+    //   host: "127.0.0.1",
+    //   port: 8545,
+    //   network_id: "*" // Match any network id
+    // },
+
+    rinkeby: {
+      provider: () => new HDWalletProvider("grain urge settle floor large actual banana cave trap hole antenna tail", "rinkeby.infura.io/v3/bccef2632c00453b9f32337cc72b779a"),
+      network_id: 3,       // Ropsten's id
+    },
+  },
+
+
 };
