7f24c9e29111b73a4aa417f25a785caf7675a0af
Saly <saly@juke4me.com>
2019-06-02 05:51:50 +0530

    Add ReadMe

---
+++ Readme.md
@@ -0,0 +1,21 @@
+# FT-Coffee
+
+FT-Coffee is a supply chain DApp, that manage user permissions and track a product’s authenticity.
+
+## Preparation 
+
+
+![AD](./uml/UML1_AD.png)
+![SD](./uml/UML2_SD.png)
+![SD](./uml/UML3_SD.png)
+![DM](./uml/UML4_DM.png)
+
+## Contract Address
+
+0x233710Fc29Be5d40506fF629fD3BD538530C0Ce6
+
+## Built With
+
+Ethereum - Ethereum is a decentralized platform that runs smart contracts
+Truffle Framework - Truffle is the most popular development framework for Ethereum with a mission to make your life a whole lot easier. v5.0.17 (core: 5.0.16)
+Node - v8.11.2
--- truffle.js
+++ truffle.js
@@ -2,14 +2,14 @@ const HDWalletProvider = require('truffle-hdwallet-provider');
 
 module.exports = {
   networks: {
-    // development: {
-    //   host: "127.0.0.1",
-    //   port: 8545,
-    //   network_id: "*" // Match any network id
-    // },
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
 
     rinkeby: {
-      provider: () => new HDWalletProvider("grain urge settle floor large actual banana cave trap hole antenna tail", "rinkeby.infura.io/v3/bccef2632c00453b9f32337cc72b779a"),
+      provider: () => new HDWalletProvider("grain urge settle floor large actual banana cave trap hole antenna tail", "rinkeby.infura.io/v3/08b91d9cb5bc43328b9c59b6c6e6f904"),
       network_id: 3,       // Ropsten's id
     },
   },
